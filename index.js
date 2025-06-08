1a3c73a60ea11ab72ff879092a6486ab0743c7c5
asguerrero <valentinaguerrero.as@hotmail.com>
2025-06-08 17:57:26 -0500

    adding local files

---
+++ index.js
@@ -0,0 +1,11 @@
+
+// import $ from "https://cdn.skypack.dev/cash-dom";
+
+$(document).ready(function(){
+
+    $(".open-button").on("click", function (event) {
+        $(".pop-up-container").css("display", "flex");
+        console.log("it's working");
+      });
+  
+});
